c76d3af377a9ef7f34d146ce69cb8617f4d6a826
limingshu <limingshu@baidu.com>
2021-01-05 12:28:34 +0000

    01-05 version

--- src/main.cu
+++ src/main.cu
@@ -19,19 +19,19 @@ int main(int argc, char *argv[])
     N = 1<<offset;
     
     // case1 : Query device info.
-    // ret = device_test();
+    ret = device_test();
 
     // case4 : Perf_test for single_validation 
     ret = perf_test_for_single_validation<DATA_TYPE>(N, loop);
 
     // case3 : Perf_test with combination block thread
-    // ret = perf_test_with_combination_block_thread<DATA_TYPE>(N, loop);
+    ret = perf_test_with_combination_block_thread<DATA_TYPE>(N, loop);
 
     // case4 : Perf_test with memory address offset.
-    // ret = perf_test_with_mem_addr<DATA_TYPE>(N, loop);
+    ret = perf_test_with_mem_addr<DATA_TYPE>(N, loop);
     
     // case5 : Perf_test with multi-stream
-    // ret = perf_test_with_stream<DATA_TYPE>(N, loop);
+    ret = perf_test_with_stream<DATA_TYPE>(N, loop);
 
     return 0;
 }
