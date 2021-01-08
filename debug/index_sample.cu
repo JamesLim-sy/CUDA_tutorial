cf750f71cf64ec371ffebbce52f3036ae53a8ea7
limingshu <limingshu@baidu.com>
2021-01-08 12:43:14 +0000

    01-08 modified the bug attributed to mess pitch/stride of data and index.

--- debug/index_sample.cu
+++ debug/index_sample.cu
@@ -8,70 +8,39 @@
 using namespace std;
 
 
-// template <typename T>
-// __global__ void index_kernel(T *p_index, T *p_input, T *p_output, 
-//                              size_t pitch, size_t width, size_t height) 
-// { 
-//     int   ix = blockDim.x * blockIdx.x + threadIdx.x;
-//     int   iy = blockDim.y * blockIdx.y + threadIdx.y;
-//     int   tid    = iy * 128 + ix;
-//     int   tid_x  = tid;
-//     int   tid_y  = iy * 128 + ix;
-    
-//     // int  *p_src = reinterpret_cast<int *>(p_input);
-//     // int  *p_idx = reinterpret_cast<int *>(p_index);
-//     // int  *p_dst = reinterpret_cast<int *>(p_output);
-
-//     // for (int i = tid; i < (width>>2) * height; i += (width>>2)) 
-//     // if (ix < (width))
-//     if (ix < width & iy < height)
-//     {
-//         // int4 idx_i4 = p_idx[tid];
-//         // int4 out_i4;
-//         // int  *idx_i = reinterpret_cast<int *>(&idx_i4);
-//         // int  *out_i = reinterpret_cast<int *>(&out_i4);
-
-//         // out_i[0] = p_src[0];
-//         // out_i[1] = p_src[1];
-//         // out_i[2] = p_src[2];
-//         // out_i[3] = p_src[3];     
-//         // p_dst[tid] = out_i4;
-//         // int *p_src = &(p_input[tid]);
-//         int idx    = p_index[tid];
-//         int *p_src = &(p_input[tid]);
-//         p_output[tid] = p_src[idx];//p_input[idx];
-//         printf("tid : %d  \tidx : %d  \t p_input: %d  \tp_src : %d\n",
-//                tid, idx, p_input[tid_y], p_src[idx]);
-//     }
-// }
-template <typename T>
-__global__ void index_kernel(T *p_index, T *p_input, T *p_output, 
-                             size_t pitch, size_t width, size_t height) 
-{ 
-    int   ix = blockDim.x * blockIdx.x + threadIdx.x;
-    int   iy = blockDim.y * blockIdx.y + threadIdx.y;
-    int   tid    = iy * 128 + ix;
-    int   tid_x  = tid;
-    int   tid_y  = iy * width + ix;
-    
-    if (ix < width & iy < height)
-    {
-        int idx         = p_index[tid];
-        p_output[tid_y] = p_input[tid - ix + idx];
-    }
-}
-
-
-#define T int
-#ifndef ALIGNED
+#define T     int
+#define Tndex int 
 #define ALIGNED(val, align_)  ((val + align_ - 1) & (~(align_ - 1)))
-#endif
 #define CHECK_ERR(condi, val)  do{ \
     if(condi) {\
         fprintf(stderr, "[%s %d]: CUDA Runtime Error: %d\n", __func__, __LINE__, ret);\
         return ret; \
     }\
 }while(0);
+
+
+template <typename T, typename IndexT=int >
+__global__ void index_kernel(IndexT *p_index, T *p_input, T *p_output, 
+                             size_t pitch_idx,
+                             size_t pitch_src, 
+                             size_t pitch_dst
+                             size_t width_index, 
+                             size_t height_index) 
+{ 
+    int   ix = blockDim.x * blockIdx.x + threadIdx.x;
+    int   iy = blockDim.y * blockIdx.y + threadIdx.y;
+    int   tid    = iy * pitch_idx + ix;
+    int   tid_x  = ty * pitch_src + ix;
+    int   tid_y  = iy * pitch_dst + ix;
+    
+    if (ix < width_index & iy < height_index)
+    {
+        T *idx          = p_index[tid];
+        p_output[tid_y] = p_input[tid_x - ix + idx];
+    }
+}
+
+
 
 int main()
 {
@@ -137,13 +106,9 @@ int main()
 
     ret = cudaMalloc((void **)&(g_output), batch_size * width_index * sizeof(T));
     CHECK_ERR(ret != (cudaError_t)0, ret);
-    
-    // cout <<  "width_input  : " << width_input << endl <<  "width_output : " << width_output << endl;
-    // cout <<  "pitch_input  : " << pitch_input << endl <<  "pitch_output : " << pitch_output << endl;
-    // cout << g_input << endl;
+
 
     pitch_input = width_input > pitch_input ? width_input : pitch_input;
-
     std::vector<T> test(batch_size * (width_value + width_index) );
     std::vector<T> dst(batch_size * (width_index));
     
@@ -183,19 +148,12 @@ int main()
                    (batch_size  + block_dim.y - 1) / block_dim.y);
     cout << block_x << "\t" << thread_x << "\t" << batch_size << endl;
 
-    // index_kernel <<<gird_dim, block_dim, 0, stream_id >>>(g_input,
-    //                                                       ((T *)(size_t(g_input) + width_output)),
-    //                                                       g_output,
-    //                                                       pitch_input,
-    //                                                       width_index,  
-    //                                                       batch_size);
     index_kernel <<<gird_dim, block_dim, 0, stream_id >>>(g_input,
                                                           ((T *)(size_t(g_input) + width_output)),
                                                           g_output,
                                                           pitch_input,
                                                           width_index,  
                                                           batch_size);
-
 
     ret = cudaMemcpy2D(static_cast<void*>(dst.data()), 
                        width_output,
@@ -217,8 +175,4 @@ int main()
 
     cudaFree(g_input);
     cudaFree(g_output);
-    
 }
-
-
-
