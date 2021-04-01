80682d958f78a890449ccc7723093b5c71e27c93
JamesLim-sy <santamonic@sina.com>
2021-04-01 06:09:00 +0000

    Fix bugs

--- test/test_scripts.cpp
+++ test/test_scripts.cpp
@@ -4,34 +4,38 @@
 #include <algorithm>
 
 template <typename vec_t>
-void functor(vec_t * vec_data) {
-    for (int i : *vec_data) { std::cout << i << "\t";}
-    std::cout << std::endl;
-}
-
-
-/*
-* 5. [32, 2, 16, 12] + [32, 2 ] => [64, 192]    + [64, 1]
-* 6. [32, 2, 16, 12] + [16, 12] => [32, 2, 192] + [1, 1, 192]
-*/
-template <int N, typename vec_t>
 void MergeDims(std::vector<vec_t> * a, vec_t *c) {
     auto out_size = c->size();
-    std::vector<int> v_idx(N, 0);
-    
-    #pragma unroll 
+    std::vector<int> vec_temp(out_size);  
+/*
+    vec_t a {1,5,4};
+    vec_t b {2};
+    vec_t c {2,5,1};
+    vec_t d {3,1,5};
+    vec_t e {3,2};
+    vec_t f {2,5};
+    vec_t g {5,4};
+
+       out  {3,2,5,4}; 
+*/
     for (vec_t v : *a) {
         std::reverse(v.begin(), v.end());
-
+    
         if (v.size() != out_size ) {
-            std::fill(v_idx.begin(), v_idx.end(), 0);
+            std::fill(vec_temp.begin(), vec_temp.end(), 1);
             int idx_in = 0, idx_out = 0;
+            v.reserve(out_size);
             do {
-                
-
-
-            }while(0);
+                if (v[idx_in] == (*c)[idx_out] ||
+                    v[idx_in] == 1 ) {
+                    vec_temp[idx_out++] = v[idx_in++];
+                } else {
+                    idx_out++;
+                }
+            }while(idx_out < out_size);
+            v.assign(vec_temp.begin(), vec_temp.end());
         }
+        for (int i=0; i < out_size; ++i) { std::cout << v[i] << "\t"; } std::cout << std::endl;
     }
 }
 
@@ -53,33 +57,26 @@ int main() {
                     } };
     vec_push(&vec_data, num);         
 
-    //3. for 循环测试
-    // for (int i : vec_data) { std::cout << i << "\t";}
-    std::cout << std::endl;
-
-    //4. std::reverse测试, 需要配合 #include <algorithm>
-    std::reverse(vec_data.begin(), vec_data.end());
-    // for (int i : vec_data) { std::cout << i << "\t";}
-    std::cout << std::endl;
-
-    //5. 传参测试
-    functor(&vec_data);
-
     //6. 模板元编译完成 switch 操作
 
-
     //7. 维度合并测试
-    vec_t a{3,1,5,4};
+    vec_t a{1,5,4};
     vec_t b{2};
-    vec_t c{3,2,5,4};
-    std::reverse(c.begin(), c.end());
+    vec_t c{2,5,1};
+    vec_t d{3,1,5};
+    vec_t e{3,2};
+    vec_t f{2,5};
+    vec_t g{5,4};
+    vec_t out{3,2,5,4};
+    std::reverse(out.begin(), out.end());
 
-    std::vector<vec_t>in_arr;
-    in_arr.reserve(2);
-    in_arr.emplace_back(a);
-    in_arr.emplace_back(b);
-    MergeDims<2, vec_t>(&in_arr, &c);
-
-
-
+    std::vector<vec_t> in_arr;
+    // in_arr.emplace_back(a);
+    // in_arr.emplace_back(b);
+    // in_arr.emplace_back(c);
+    // in_arr.emplace_back(d);
+    in_arr.emplace_back(e);
+    // in_arr.emplace_back(f);
+    in_arr.emplace_back(g);
+    MergeDims<vec_t>(&in_arr, &out);
 }
